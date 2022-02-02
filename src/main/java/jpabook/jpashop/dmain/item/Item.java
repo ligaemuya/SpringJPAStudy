5a461b4e24145b4d97bad26bbe7f734d8b25c61e
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-02-02 19:56:27 +0900

    엔티티 클래스개발 2

--- src/main/java/jpabook/jpashop/dmain/item/Item.java
+++ src/main/java/jpabook/jpashop/dmain/item/Item.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop.dmain.item;
 
+import jpabook.jpashop.dmain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -21,5 +24,8 @@ public class Item {
     private int price;
 
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 }
