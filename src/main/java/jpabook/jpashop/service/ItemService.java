ad30d477eeb5c62e8e90b08fdd17dcc27ee9f4a1
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-02-15 00:09:50 +0900

    수정

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,13 +1,11 @@
 package jpabook.jpashop.service;
 
-import jpabook.jpashop.dmain.*;
 import jpabook.jpashop.dmain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -30,17 +28,5 @@ public class ItemService {
         return itemRepository.findOne(itemId);
     }
 
-    //== 생성 메서드 ==//
-    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
-        Order order = new Order();
-        order.setMember(member);
-        order.setDelivery(delivery);
-        for (OrderItem orderItem : orderItems) {
-            order.addOrderItem(orderItem);
-        }
-        order.setStatus(OrderStatus.ORDER);
-        order.setOrderDate(LocalDateTime.now());
-        return order;
-    }
 
 }
